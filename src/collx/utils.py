8cd484603aebc93cf6d92567890def772bd95e8d
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-05-26 15:41:35 -0400

    chore: lint and typehints

--- src/collx/utils.py
+++ src/collx/utils.py
@@ -3,9 +3,10 @@ from pathlib import Path
 PROJECT_NAME = "collx"
 
 
-def capitalize_separated_name(name):
+def capitalize_separated_name(name: str) -> str:
     return " ".join([part_name.capitalize() for part_name in name.split()])
 
 
-def get_config_folder_path():
+def get_config_folder_path() -> str:
+    """For use in configuring logging"""
     return Path(Path(".").parent.parent.parent, "config")
