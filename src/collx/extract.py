0f015a61dfa3880db0a2c01dad6fd7233e06c629
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-05-25 21:53:48 -0400

    refactor: return ids in queries

--- src/collx/extract.py
+++ src/collx/extract.py
@@ -1,6 +1,6 @@
 import datetime as dt
 import re
-from typing import Dict, Optional, Union
+from typing import Dict, Optional, List, Tuple, Union
 
 from sqlalchemy.sql.expression import select
 from thefuzz import fuzz
@@ -21,24 +21,38 @@ def query_for_double():
 def _query_for_set_name(name):
     with get_engine().begin() as conn:
         return conn.execute(
-            select(SET_TABLE.c.name)
+            select(SET_TABLE.c.name, SET_TABLE.c.id)
             .where(SET_TABLE.c.name.like(f"%{name}%"))
             .distinct()
-            .limit(10)
         ).all()
 
 
 def _query_for_player_name(name):
     with get_engine().begin() as conn:
         return conn.execute(
-            select(PLAYER_TABLE.c.name)
+            select(PLAYER_TABLE.c.name, PLAYER_TABLE.c.id)
             .where(PLAYER_TABLE.c.name.like(f"%{name}%"))
             .distinct()
-            .limit(10)
         ).all()
 
 
-def _extract_player_from_title(title: str) -> Optional[str]:
+def _validate_item_from_matches(
+    item: str, db_matches: List[Tuple[str, int]]
+) -> Optional[str]:
+    ordered_db_matches = sorted(
+        [
+            (*db_match[0:2], fuzz.token_sort_ratio(item, db_match[0]))
+            for db_match in db_matches
+        ],
+        key=lambda info: info[-1],
+        reverse=True,
+    )
+    if ordered_db_matches:
+        return ordered_db_matches[0][0:2]
+    return [None, None]
+
+
+def _extract_possible_player_from_title(title: str) -> Optional[str]:
     name_pattern = r"[^\d\W]+\s+[^\d\W]+"
     name_match = None
     name_db_matches = []
@@ -54,20 +68,10 @@ def _extract_player_from_title(title: str) -> Optional[str]:
             and len(split_title) > 2
             and " ".join(split_title[1:])
         )
-    ordered_db_matches = sorted(
-        [
-            (name_db_match[0], fuzz.token_sort_ratio(name_match, name_db_match[0]))
-            for name_db_match in name_db_matches
-        ],
-        key=lambda info: info[-1],
-        reverse=True,
-    )
-    if ordered_db_matches:
-        return ordered_db_matches[0][0]
-    return None
+    return name_match, name_db_matches
 
 
-def _extract_set_from_title(title: str, year: int) -> Optional[str]:
+def _extract_possible_set_from_title(title: str, year: int) -> Optional[str]:
     # XXX: Refactor to use sliding window as with extract_player
     card_set_pattern = rf"{year}\s?\w+"
     card_set_name = None
@@ -82,17 +86,7 @@ def _extract_set_from_title(title: str, year: int) -> Optional[str]:
         else:
             break
         card_set_pattern += r"\s+\w+"
-    ordered_db_matches = sorted(
-        [
-            (set_db_match[0], fuzz.token_sort_ratio(card_set_name, set_db_match[0]))
-            for set_db_match in set_db_matches
-        ],
-        key=lambda info: info[-1],
-        reverse=True,
-    )
-    if ordered_db_matches:
-        return ordered_db_matches[0][0]  # name of closest match
-    return None
+    return card_set_name, set_db_matches
 
 
 def _extract_year_from_title(title: str) -> Optional[int]:
@@ -103,7 +97,6 @@ def _extract_year_from_title(title: str) -> Optional[int]:
     return None
 
 
-# def _extract_player_from_title(title: str) -> Optional[str]:
 def extract_data(
     data: Dict[str, Optional[Union[str, int, float, bool]]]
 ) -> Dict[str, Optional[Union[str, int, float, bool]]]:
@@ -113,27 +106,58 @@ def extract_data(
     Note: If there is a high chance of an extraction being a false positive, we avoid
     returning anything for said key, value.
     """
+    card_set_name = card_set_id = None
+    card_player_name = card_player_id = None
     if not data and data.get("title"):
         raise ValueError(f"No title in data: {data}")
 
     parsed_title = data.get("title").lower()
     if not (
         card_year := (epoch := data.get("date_stamp"))
-        and dt.datetime.fromtimestamp(epoch).year
+        and (converted_year := dt.datetime.fromtimestamp(epoch).year)
+        and str(converted_year)
     ):
         card_year = _extract_year_from_title(parsed_title)
-    if not (card_set_name := data.get("attributes", {}).get("Set")):
-        if card_year:
-            card_set_name = _extract_set_from_title(parsed_title, card_year)
+
+    if possible_card_set_name := data.get("attributes", {}).get(
+        "Set"
+    ):  # This may not match what's in db
+        set_db_matches = _query_for_set_name(possible_card_set_name)
+    elif card_year:
+        possible_card_set_name, set_db_matches = _extract_possible_set_from_title(
+            parsed_title, card_year
+        )
+    else:
+        set_db_matches = []
+
+    if set_db_matches:
+        card_set_name, card_set_id = _validate_item_from_matches(
+            possible_card_set_name, set_db_matches
+        )
+
     if card_set_name:
         parsed_title = parsed_title.replace(card_set_name.lower(), "")
     elif card_year:
         parsed_title = parsed_title.replace(card_year, "")
-    if not (card_player_name := data.get("attributes", {}).get("Player/Athlete")):
-        card_player_name = _extract_player_from_title(parsed_title)
+
+    if possible_card_player_name := data.get("attributes", {}).get(
+        "Player/Athlete"
+    ):  # This may not match what's in db
+        player_db_matches = _query_for_player_name(possible_card_player_name)
+    else:
+        (
+            possible_card_player_name,
+            player_db_matches,
+        ) = _extract_possible_player_from_title(parsed_title)
+    if player_db_matches:
+        card_player_name, card_player_id = _validate_item_from_matches(
+            possible_card_player_name, player_db_matches
+        )
 
     return {
         "year": card_year and int(card_year),
         "set": card_set_name and capitalize_separated_name(card_set_name),
+        "set_id": card_set_id,
         "player": card_player_name and capitalize_separated_name(card_player_name),
+        "player_id": card_player_id,
     }
--- src/collx/utils.py
+++ src/collx/utils.py
@@ -1,2 +1,11 @@
+from pathlib import Path
+
+PROJECT_NAME = "collx"
+
+
 def capitalize_separated_name(name):
     return " ".join([part_name.capitalize() for part_name in name.split()])
+
+
+def get_config_folder_path():
+    return Path(Path(".").parent.parent.parent, "config")
