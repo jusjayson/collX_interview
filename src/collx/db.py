9eebfb7318e7a83460c1ea69e69e709d7f99962c
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-05-25 21:56:33 -0400

    fix: missing table columns

--- src/collx/db.py
+++ src/collx/db.py
@@ -15,10 +15,18 @@ metadata_obj = MetaData()
 CARD_TABLE = Table(
     "cards",
     metadata_obj,
+    Column("id", Integer, primary_key=True),
     Column("name", String(254)),
-    Column("set_id", ForeignKey("cards.id")),
+    Column("set_id", ForeignKey("cards.set_id")),
+    Column("player_id", ForeignKey("cards.player_id")),
+    Column("year", Integer()),
 )
-PLAYER_TABLE = Table("players", metadata_obj, Column("name", String(254)))
+PLAYER_TABLE = Table(
+    "players",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("name", String(254)),
+)
 SET_TABLE = Table(
     "sets",
     metadata_obj,
