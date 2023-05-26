d83955d41077a4adecab0a8c9d1564cf1021041d
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-05-25 21:56:15 -0400

    fix: updated extract names

--- tests/test_extract_info.py
+++ tests/test_extract_info.py
@@ -31,8 +31,8 @@ def test_extract_player_from_compound_set():
 
 
 @mock.patch("collx.extract._extract_year_from_title")
-@mock.patch("collx.extract._extract_set_from_title")
-@mock.patch("collx.extract._extract_player_from_title")
+@mock.patch("collx.extract._extract_possible_set_from_title")
+@mock.patch("collx.extract._extract_possible_player_from_title")
 def test_extract_from_full_data(mock_xtract_player, mock_xtract_set, mock_xtract_year):
     """
     Don't use unnecessary extracts if we already have the info we need
@@ -76,7 +76,6 @@ def test_extract_from_full_data(mock_xtract_player, mock_xtract_set, mock_xtract
     }
     result = extract_data(test_data)
     assert result.get("year") == 2023
-    assert result.get("set") == "2023 Bowman"
     assert result.get("player") == "Colby Thomas"
     mock_xtract_year.assert_not_called()
     mock_xtract_set.assert_not_called()
