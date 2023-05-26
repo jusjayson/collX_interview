bc8f33e1de26ce6dc322adfd91476f62884d9df3
Jayson Dorsett <ja_dorsett@jdorsett.com>
2023-05-26 15:41:05 -0400

    chore: lint

--- src/collx/match.py
+++ src/collx/match.py
@@ -1,8 +1,8 @@
 from typing import Dict, Optional, Union
 
-from sqlalchemy.sql.expression import join, select
+from sqlalchemy.sql.expression import select
 
-from collx.db import get_engine, CARD_TABLE, PLAYER_TABLE, SET_TABLE
+from collx.db import get_engine, CARD_TABLE
 
 
 def match_card_info_to_db(card_data: Dict[str, Optional[Union[str, int]]]):
@@ -10,10 +10,11 @@ def match_card_info_to_db(card_data: Dict[str, Optional[Union[str, int]]]):
     card_year = card_data.get("year")
     set_id = card_data.get("set_id")
     with get_engine().begin() as conn:
-        return conn.execute(
+        match = conn.execute(
             select(CARD_TABLE.c.id, CARD_TABLE.c.name).where(
                 CARD_TABLE.c.set_id == set_id,
                 CARD_TABLE.c.year == card_year,
                 CARD_TABLE.c.player_id == player_id,
             )
-        ).first()  # Could be more than one
+        ).first()
+        return match
